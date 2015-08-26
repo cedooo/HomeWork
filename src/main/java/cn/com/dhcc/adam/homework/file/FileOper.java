3d94f2e7d5ac227d13d8a86ffbf99c45119b9c06
cedoo <cedoo@qq.com>
2015-08-26 15:53:45 +0800

    read file by NIO

--- src/main/java/cn/com/dhcc/adam/homework/file/FileOper.java
+++ src/main/java/cn/com/dhcc/adam/homework/file/FileOper.java
@@ -2,9 +2,12 @@ package cn.com.dhcc.adam.homework.file;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.logging.Logger;
 
 public class FileOper {
@@ -51,7 +54,43 @@ public class FileOper {
 				}
 			}
 		}
-		
+		readByNIO();
 	}
 
+	private static void readByNIO(){
+		System.out.println("==========NIO=================");
+		String proPath = System.getProperty("user.dir");
+		//System.out.println(System.getProperty("user.home"));
+		String filePath = proPath + "/src/main/java/cn/com/dhcc/adam/homework/file/testfile.txt";
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			FileChannel fileChannel = fis.getChannel();
+			int capacity = 12;
+			ByteBuffer bytebuff = ByteBuffer.allocate(capacity);
+			byte[] charbyte = new byte[capacity];
+			StringBuilder strbu = new StringBuilder();
+			int length;
+			
+			while((length = fileChannel.read(bytebuff))!=-1){
+				//System.out.println(length  + ":params:" + bytebuff.limit() + "," + bytebuff.capacity() + "," + bytebuff.position());
+				bytebuff.flip();    //flip()方法将Buffer从写模式切换到读模式
+				bytebuff.get(charbyte, 0 , length);
+				
+				String s = new String(charbyte);
+//				System.out.println(s);
+				
+				strbu.append(s);
+				bytebuff.clear();
+				charbyte = new byte[capacity];
+			}
+			
+			System.out.println("string builder length:" + strbu.length());
+			System.out.println(strbu);
+			fis.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
