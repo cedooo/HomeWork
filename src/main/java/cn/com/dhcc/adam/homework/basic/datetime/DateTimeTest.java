0896ada669f7d848d833c0b8baba942c08e5f82b
cedoo <cedoo@qq.com>
2015-09-29 17:30:23 +0800

    jdk8 DateTime API (1): LocalDateTime, LocalDate, ZonedDateTime, ZoneId, YearMonth, MonthDay, Year, Instant, DayOfWeek, Month, LocalTime

--- src/main/java/cn/com/dhcc/adam/homework/basic/datetime/DateTimeTest.java
+++ src/main/java/cn/com/dhcc/adam/homework/basic/datetime/DateTimeTest.java
@@ -1,13 +1,17 @@
 package cn.com.dhcc.adam.homework.basic.datetime;
 
+import java.time.Clock;
 import java.time.DayOfWeek;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Set;
 
 public class DateTimeTest {
 
@@ -42,6 +46,25 @@ public class DateTimeTest {
 		//判断某年2月有多少天
 		YearMonth ym = YearMonth.of(2009, Month.FEBRUARY);
 		System.out.println(ym.isLeapYear() + ":" + ym.lengthOfMonth());
+		
+		System.out.println(ZoneId.systemDefault());
+		//时区
+		Set<String> allZoneId = ZoneId.getAvailableZoneIds();
+		for (String zoneId : allZoneId) {
+			System.out.println(zoneId);
+		}
+		
+		
+		Instant nowIns = Instant.now();
+		LocalDateTime ldt = LocalDateTime.ofInstant(nowIns, ZoneId.systemDefault());
+		System.out.println(ldt);
+		ZoneId losA = ZoneId.of("America/Los_Angeles");
+		LocalDateTime losldt = LocalDateTime.ofInstant(nowIns, losA);
+		System.out.println(losldt);
+		
+		//Clock
+		Clock nowClock = Clock.systemUTC();
+		System.out.println(nowClock);
 	}
 
 }
--- src/main/java/cn/com/dhcc/adam/homework/basic/datetime/README.md
+++ src/main/java/cn/com/dhcc/adam/homework/basic/datetime/README.md
@@ -3,5 +3,5 @@
 	<li>The Date-Time API uses the calendar system defined in ISO-8601 as the default calendar</li>
 	<li><a class="OutsideLink" target="_blank" href="http://cldr.unicode.org">Unicode Common Locale Data Repository (CLDR)</a></li>
 	<li><a class="OutsideLink" target="_blank" href="http://www.iana.org/time-zones">Time-Zone Database (TZDB)</a></li>
-	<li> human time | machine time</li>
+	<li> human time(...Date)| machine time(Instant)</li>
 </ul>
