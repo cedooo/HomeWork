2c13e747e577edc845e4fd7bdfb9670aaf2f10dd
cedoo <cedoo@qq.com>
2016-10-11 16:16:29 +0800

    last

--- src/main/java/javas/his/jdk8/Executor.java
+++ src/main/java/javas/his/jdk8/Executor.java
@@ -3,6 +3,8 @@ package javas.his.jdk8;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by cedo on 2016/9/21.
  * 多线程执行
@@ -26,17 +28,29 @@ public class Executor {
         executor.execute(()->log.debug(Thread.currentThread().getName() + " start "));
 
 
-        ((java.util.concurrent.Executor)((command) -> {
-            Thread t = new Thread(command);
-            t.start();
-        })).execute(()->log.debug(Thread.currentThread().getName() + " start "));
+        ((java.util.concurrent.Executor)
+                command -> new Thread(command).start()
+        ).execute(()->log.debug(Thread.currentThread().getName() + " start "));
+
 
 
         java.util.concurrent.Executors.newSingleThreadExecutor().execute(()->log.info(Thread.currentThread().getName() + " start "));
+
+
+
+        Runnable r1 = ()->log.info(Thread.currentThread().getName());
+        Runnable r2 = ()->log.info(Thread.currentThread().getName());
+
+        java.util.concurrent.ScheduledExecutorService threadPool = java.util.concurrent.Executors.newScheduledThreadPool(10);
+        threadPool.scheduleAtFixedRate(r1, 3, 3, TimeUnit.SECONDS);
+        threadPool.scheduleAtFixedRate(r2, 2, 4, TimeUnit.SECONDS);
+
         try {
-            Thread.sleep(1000);
+            Thread.sleep(100*1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
     }
+
 }
--- src/main/java/javas/his/jdk8/LambdaExpression.java
+++ src/main/java/javas/his/jdk8/LambdaExpression.java
@@ -9,62 +9,58 @@ import static java.util.Arrays.asList;
  * 正则表达式
  */
 public class LambdaExpression {
-    public static java.util.List<Person> li = new java.util.ArrayList<>();
 
     public static void main(String[] args) {
         new Thread(() -> out.println(new java.util.Date())).start();
 
-        java.util.List<String> input = asList(new String[]{"apple", "orange", "pear"});
+        java.util.List<String> input = asList("apple", "orange", "pear");
         input.forEach(out::println);
 
         java.util.List<Person> roster = asList(new Person("cedo", 27), new Person("dhc", 10), new Person("test", 900), new Person("chen", 64));
 
-        processPersons(roster, p -> p.getAge() > -1 && p.getAge() < 80, p -> out.println(p));
+        processPersons(roster, p -> p.getAge() > -1 && p.getAge() < 80, out::println);
 
         coustomer();
     }
 
-    public static void processPersons(
+    private static void processPersons(
             java.util.List<Person> roster,
             java.util.function.Predicate<Person> tester,
             java.util.function.Consumer<Person> block) {
+        /*
         for (Person p : roster) {
             if (tester.test(p)) {
                 block.accept(p);
             }
         }
+        */
+        roster.forEach(p->{
+                if(tester.test(p)) block.accept(p);
+            }
+        );
+
     }
 
-    static public void coustomer() {
+    static private void coustomer() {
         int x = 13;
-        java.util.function.Consumer<Integer> cus = (i) -> {
-            System.out.printf("i= %d \n", i);
-        };
-        cus.accept((Integer) x);
+        java.util.function.Consumer<Integer> cus = (i ->System.out.printf("i= %d \n", i));
+        cus.accept(x);
     }
 }
 
 class Person {
     public String name;
-    public int age;
+    private int age;
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
+    int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public Person(String name, int age) {
+    Person(String name, int age) {
         Objects.requireNonNull(name);
         this.name = name;
         this.age = (age >= 0 && age < 200) ? age : 0;
