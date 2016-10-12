11e37dfb8b60bf384e7508c16971d6a16a547f4a
cedoo <cedoo@qq.com>
2016-10-12 17:35:02 +0800

    SYNC

--- src/main/java/cedo/utils/DownT66y.java
+++ src/main/java/cedo/utils/DownT66y.java
@@ -1,6 +1,13 @@
 package cedo.utils;
 
+import javax.swing.text.html.HTML;
+import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.System.out;
 /**
@@ -9,15 +16,15 @@ import static java.lang.System.out;
  */
 public class DownT66y {
 
-    static final private String T66Y_URL ="HTTP://baidu.com";
+    static final private String T66Y_URL ="http://commons.apache.org/proper/commons-net/";
     static private final String PROXY_IP = "192.168.1.1";
     static private final int PROXY_PORT = 99;
 
-    static private java.net.URL t66yUrl;
-    static private java.net.Proxy proxy;
+    static private URL t66yUrl;
+    static private Proxy proxy;
     static {
         try {
-            t66yUrl = new java.net.URL(T66Y_URL);
+            t66yUrl = new URL(T66Y_URL);
 
             byte[] addr = new byte[4];
             String[] ipAddr = PROXY_IP.split("\\.");
@@ -25,11 +32,11 @@ public class DownT66y {
                 addr[i] = (byte)(Integer.parseInt(ipAddr[i]) & 0xff);
             }
             out.println(PROXY_IP + " <=> " + java.util.Arrays.toString(addr));
-            java.net.InetAddress inetAddress = java.net.InetAddress.getByAddress(addr);
-            java.net.SocketAddress socketAddress = new java.net.InetSocketAddress(inetAddress, PROXY_PORT);
+            InetAddress inetAddress = InetAddress.getByAddress(addr);
+            SocketAddress socketAddress = new InetSocketAddress(inetAddress, PROXY_PORT);
 
-            proxy = new java.net.Proxy(java.net.Proxy.Type.SOCKS, socketAddress);
-        } catch (java.net.MalformedURLException e) {
+            proxy = new Proxy(Proxy.Type.SOCKS, socketAddress);
+        } catch  (MalformedURLException e) {
             e.printStackTrace();
         } catch (Exception e) {
             out.printf("address maybe wrong!");
@@ -38,16 +45,55 @@ public class DownT66y {
 
     static public void main(String[] args){
         try {
-            java.net.URLConnection t66yConn = t66yUrl.openConnection(proxy);
-            java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.InputStreamReader(t66yConn.getInputStream()));
-            String line;
-            while((line = bufferedReader.readLine())!=null){
-                out.printf(line);
-            }
+            boolean saved = Spider.saveHTML(t66yUrl.openStream(),"temp/aa.html");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+
+
+}
+
+/**
+ * 简单的爬虫
+ */
+class Spider implements Runnable{
+
+    private List<HTMLLink> htmlLib = new ArrayList<>();
+
+    @Override
+    public void run() {
+
+    }
+
+    static boolean saveHTML(java.io.InputStream inStream, String fileName){
+        java.io.File file = new java.io.File(fileName);
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter(file);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inStream));
+            String line;
+            while((line = bufferedReader.readLine())!=null){
+                fileWriter.append(line);
+            }
+            fileWriter.flush();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            try {
+                if (fileWriter != null) {
+                    fileWriter.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
+
+class HTMLLink {
 
 }
